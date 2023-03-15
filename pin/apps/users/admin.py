717d5f3cb18ec8c63651f65f81622e7cba35d2d3
fatahrez <fertahrez@gmail.com>
2023-03-16 02:36:04 +0300

    custom User for admin

--- pin/apps/users/admin.py
+++ pin/apps/users/admin.py
@@ -1,3 +1,95 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+from .forms import CustomUserRegisterForm, CustomUserChangeForm
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['email']
+    add_form = CustomUserRegisterForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = [
+        'pkid', 
+        'id',
+        'email', 
+        'username', 
+        'first_name',
+        'is_staff', 
+        'is_active'
+    ]
+    list_display_links = [
+        'id', 
+        'email'
+    ]
+    list_filter = [
+        'email', 
+        'username',
+        'first_name',
+        'is_staff',
+        'is_active'
+    ]
+    fieldsets = (
+        (
+            _("Login Credentials"),
+            {
+                'fields': (
+                    'email', 
+                    'password',
+                )
+            },
+        ),
+        (
+            _("Personal Information"),
+            {
+                'fields': (
+                    'username', 
+                    'first_name',
+                )
+            },
+        ),
+        (
+            _("Permissions and Groups"),
+            {
+                'fields': (
+                    'is_staff', 
+                    'is_active',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                )
+            },
+        ),
+        (
+            _('Important dates'),
+            {
+                'fields': (
+                    'last_login', 
+                    'date_joined'
+                )
+            }
+        ),
+        add_fieldsets=(
+            (
+                None, {
+                    'classes': ('wide',),
+                    'fields': (
+                        'email', 
+                        'password1', 
+                        'password2',
+                        'is_staff',
+                        'is_active',
+                    ),
+                },
+            ),
+        ),
+    )
+    search_fields= [
+        'email',
+        'username',
+        'first_name'
+    ]
+
+
+admin.site.register(User, UserAdmin)
