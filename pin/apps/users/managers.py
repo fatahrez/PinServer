4221125928de7f793e60f106b3110f15688e52de
fatahrez <fertahrez@gmail.com>
2023-03-16 01:55:03 +0300

    add logic for creating superuser

--- pin/apps/users/managers.py
+++ pin/apps/users/managers.py
@@ -39,7 +39,33 @@ class CustomUserManager(BaseUserManager):
 
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        user.save()
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, first_name, email, password,
+                          **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
 
 
-
+        if not password:
+            raise ValueError(_("Superuser must have a password"))
+        
+        if email:
+            email = self.normalize_email(email)
+            self.email_validator(email)
+        else:
+            raise ValueError(
+                _("Admin Account: An Email address is required")
+            )
+        user = self.create_user(username, first_name, email, password,
+                                 **extra_fields)
+        user.save(using=self._db)
+        return user
