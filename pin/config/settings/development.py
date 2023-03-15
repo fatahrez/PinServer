d02cfaba8fa262f57285443fa63066a480a59fb2
fatahrez <fertahrez@gmail.com>
2023-03-15 22:31:01 +0300

    split settings.py

---
+++ pin/config/settings/development.py
@@ -0,0 +1,15 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
+    }
+}
