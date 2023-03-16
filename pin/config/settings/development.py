1c037b9f089f0bd22c199503b68e98d38623b61d
fatahrez <fertahrez@gmail.com>
2023-03-16 12:57:23 +0300

    add email configuration in settings

--- pin/config/settings/development.py
+++ pin/config/settings/development.py
@@ -1,5 +1,16 @@
 from .base import *
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_USER_TLS = True
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = 'info@pinapp.com'
+DOMAIN = env('DOMAIN')
+SITE_NAME = "Pin App"
+
+
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
