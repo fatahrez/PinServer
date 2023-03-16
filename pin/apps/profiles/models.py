7802fbfcbdcd78e68a281786fce2c44b29f5d125
fatahrez <fertahrez@gmail.com>
2023-03-16 11:43:18 +0300

    run migrations for profile models

--- pin/apps/profiles/models.py
+++ pin/apps/profiles/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 from django_countries.fields import CountryField
 from phonenumber_field.modelfields import PhoneNumberField
 
-from apps.common.models import TimestampedUUIDModel
+from pin.apps.common.models import TimestampedUUIDModel
 
 User = get_user_model()
 
@@ -17,7 +17,7 @@ class Gender(models.TextChoices):
 class Profile(TimestampedUUIDModel):
     user = models.OneToOneField(User, related_name="profile",
                                 on_delete=models.CASCADE)
-    age = models.IntegerField(verbose_name=_("Age"), max_length=3)
+    age = models.IntegerField(verbose_name=_("Age"), default=20)
     gender = models.CharField(verbose_name=_("Gender"), max_length=20, 
                               choices=Gender.choices, default=Gender.OTHER)
     profile_photo = models.ImageField(verbose_name=_("Profile Photo"),
